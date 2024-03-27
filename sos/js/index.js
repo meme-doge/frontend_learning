3a5cb72f1cecd72ea9844d528909b4ae126f9801
root <root@void.www.digmawifi.com>
2024-03-27 16:43:19 -0400

    mostly added header for 'sos' page

--- sos/js/index.js
+++ sos/js/index.js
@@ -1,26 +1,101 @@
-const select = document.querySelector(".header__menu-lang-change");
+const select = document.querySelector(".header__menu-lang-change-lang");
+const select_change = document.querySelector(".header__menu-lang-change");
+const list = document.querySelector(".header__menu-lang-list");
+const options = document.querySelectorAll(".header__menu-lang-option");
 const AllLang = ["ENG","RUS","FRA", "DEU"]
 
-select.addEventListener("change", ListenerChanger)
+const button = document.querySelector(".header__menu-button");
+const dialog = document.querySelector(".header__dialog");
+
 ChangeLanguage();
-function ListenerChanger(){
+options.forEach((option) => {
+    option.addEventListener("click", OptionListenerClick);
+    option.addEventListener("keydown", OptionListenerKeyDown);
+});
+select_change.addEventListener("keydown", ListListenerKeyDown);
+button.addEventListener("click", ButtonListenerClick);
+function ListListenerKeyDown(){
+    if (event.keyCode === 13) {
+        list.classList.toggle('display-lang');
+    }
+}
+function OptionListenerKeyDown(){
+    if (event.keyCode === 13) {
+        let option = this.valueOf();
+
+        const url_1 = new URL(window.location.href);
+        url_1.searchParams.set('lang', option.attributes['value'].textContent);
+        let newUrl = url_1.toString();
+        window.history.pushState({path: newUrl.toString()}, '', newUrl.toString());
+
+        let select_lang = Object.assign(select.textContent);
+        let option_lang = Object.assign(option.textContent);
+
+        select.textContent = option_lang;
+        this.valueOf().textContent = select_lang;
+
+        let option_value = Object.assign(option.attributes['value'].textContent);
+        let select_value = Object.assign(select.attributes['value'].textContent);
+
+        option.setAttribute("value", select_value);
+        select.setAttribute("value", option_value);
+
+        ChangeLanguage();
+    }
+}
+function OptionListenerClick(){
+    let option = this.valueOf();
+
     const url_1 = new URL(window.location.href);
-    url_1.searchParams.set('lang', select.value);
+    url_1.searchParams.set('lang', option.attributes['value'].textContent);
     let newUrl = url_1.toString();
     window.history.pushState({path: newUrl.toString()}, '', newUrl.toString());
+
+    let select_lang = Object.assign(select.textContent);
+    let option_lang = Object.assign(option.textContent);
+
+    select.textContent = option_lang;
+    this.valueOf().textContent = select_lang;
+
+    let option_value = Object.assign(option.attributes['value'].textContent);
+    let select_value = Object.assign(select.attributes['value'].textContent);
+
+    option.setAttribute("value", select_value);
+    select.setAttribute("value", option_value);
 
     ChangeLanguage();
 }
 function ChangeLanguage(){
     const urlParams = new URLSearchParams(window.location.search);
     const lang = urlParams.get('lang');
-    console.log(lang);
-    if (AllLang.includes(lang)){
-        select.value = lang;
-        for (let key in LangArr){
-            let object = document.querySelector(".lng__" + key);
-            console.log(object)
-            object.textContent = LangArr[key][lang];
+    if (AllLang.includes(lang)) {
+        let option_;
+        options.forEach((option) => {
+            if (option.attributes['value'].textContent == lang){
+                option_ = option
+            }
+        });
+
+        if (typeof option_ == "undefined") {
+            for (let key in LangArr) {
+                let object = document.querySelector(".lng__" + key);
+                object.textContent = LangArr[key][lang];
+            }
+        }
+        else{
+            let select_lang = Object.assign(select.textContent);
+            let option_lang = Object.assign(option_.textContent);
+
+            select.textContent = option_lang;
+            option_.textContent = select_lang;
+
+            let option_value = Object.assign(option_.attributes['value'].textContent);
+            let select_value = Object.assign(select.attributes['value'].textContent);
+
+            option_.setAttribute("value", select_value);
+            select.setAttribute("value", option_value);
+
+            ChangeLanguage();
         }
     }
     else{
@@ -31,3 +106,6 @@ function ChangeLanguage(){
     }
 }
 
+function ButtonListenerClick(){
+    dialog.classList.toggle("header__dialog--open");
+}
--- sos/js/swiper.js
+++ sos/js/swiper.js
@@ -2,7 +2,7 @@ import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs
 
 const mySwiper = new Swiper('.swiper', {
     autoplay: {
-        delay: 3000,
+        delay: 300000,
     },
     direction: 'horizontal',
     loop:"true"
