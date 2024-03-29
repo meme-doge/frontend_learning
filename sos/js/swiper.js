9f68f4eba0460ef2f2ca37a4b4e6542bfa5ecf42
root <root@void.www.digmawifi.com>
2024-03-29 19:12:06 -0400

    adding a story block

--- sos/js/swiper.js
+++ sos/js/swiper.js
@@ -1,18 +1,33 @@
 import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs'
 
-const mySwiper = new Swiper('.swiper', {
+const FirstSwiper = new Swiper('.swiper-first', {
     autoplay: {
         delay: 3000,
     },
 
     navigation: {
+        nextEl: '.swiper-first-button-next',
+        prevEl: '.swiper-first-button-prev',
+    },
+    pagination: {
+        el: '.swiper-first-pagination',
+    },
+
+    direction: 'horizontal',
+})
+const SecondSwiper = new Swiper('.story__swiper', {
+    effect: "cards",
+    cardsEffect: {
+        perSlideOffset: 9,
+        perSlideRotate: 0,
+        rotate: false,
+        slideShadows: false,
+    },
+    navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
     },
     pagination: {
-        el: '.swiper-pagination',
+        el: '.story-swiper-pagination',
     },
-
-    direction: 'horizontal',
-    loop:"true"
 })
